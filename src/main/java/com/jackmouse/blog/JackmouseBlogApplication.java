7fc62d0958bb5a0cb26bbc3503061de15487eddb
znjy11 <1310179240@qq.com>
2021-03-03 14:23:50 +0800

    frist

---
+++ src/main/java/com/jackmouse/blog/JackmouseBlogApplication.java
@@ -0,0 +1,13 @@
+package com.jackmouse.blog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JackmouseBlogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JackmouseBlogApplication.class, args);
+    }
+
+}
