9d1d3978612d95fb997bdeb57d5ec947d968f0a4
znjy11 <1310179240@qq.com>
2021-03-09 19:50:53 +0800

    修改tb_ ，生成实体类，去掉表头

--- src/test/java/com/jackmouse/blog/JackmouseBlogApplicationTests.java
+++ src/test/java/com/jackmouse/blog/JackmouseBlogApplicationTests.java
@@ -1,12 +1,16 @@
 package com.jackmouse.blog;
 
+
+import com.jackmouse.blog.mapper.UserInfoMapper;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import javax.annotation.Resource;
+
 @SpringBootTest
 class JackmouseBlogApplicationTests {
-
-
+    @Resource
+    private UserInfoMapper userInfoMapper;
     @Test
     void contextLoads() {
     }
